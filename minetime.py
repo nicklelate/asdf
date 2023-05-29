eda525c7c51b9d9509b646e153cb78fa32e86e4b
nicklelate <101852708+nicklelate@users.noreply.github.com>
2023-05-29 10:22:46 +0700

    Create minetime.py

---
+++ minetime.py
@@ -0,0 +1,13 @@
+import datetime
+
+b = True
+run = 1
+while b:
+    x = datetime.datetime.now()
+    y = x.strftime("%M")
+    if y[1] == "2":
+        if run == 1:
+            print(x)
+            run = 0
+    else:
+        run = 1
